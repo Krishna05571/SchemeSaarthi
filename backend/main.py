677e9a41850bc77feedc3916b95ea771dcb891af
unknown <krishna.kaushal173@gmail.com>
2026-08-25 10:13:00 +0530

    Frontend URL changed

--- backend/main.py
+++ backend/main.py
@@ -52,7 +52,7 @@ app = FastAPI(title="AI Government Scheme Navigator API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or your Vercel URL
+    allow_origins=["https://schemesaarthi.vercel.app/"],  # or your Vercel URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
